3cb381c987afc6e8f83354d2ca19faa65398cce0
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2024-02-19 14:44:59 +0200

    Add files via upload

---
+++ Additional_task/Magazine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Additional_task
+{
+    class Magazine : Printed_matter
+    {
+        /// <summary>
+        /// Auto. fields
+        /// </summary>
+        public int Circulation {  get; set; }
+        public double Price { get; set; }
+        /// <summary>
+        /// Constructors
+        /// </summary>
+        public Magazine() { }
+        public Magazine(string name, int circulation, double price)
+        {
+            Name = name; Circulation = circulation; Price = price;
+        }
+        /// <summary>
+        /// Input
+        /// </summary>
+        /// <returns>New Magazine</returns>
+        public static Magazine Input()
+        {
+            Write("Enter the name: ");
+            string name = ReadLine();
+            Write("Enter the circulation: ");
+            int circulation = Convert.ToInt32(ReadLine());
+            Write("Enter the price for one: ");
+            double price = Convert.ToDouble(ReadLine());
+            return new Magazine(name, circulation, price);
+        }
+        /// <summary>
+        /// Overriden calculating cost
+        /// </summary>
+        /// <returns>Price for all</returns>
+        public override double Cost() => Circulation * Price;
+        /// <summary>
+        /// Output
+        /// </summary>
+        public override void Output() => WriteLine($"Name: {Name}\nCirculation: {Circulation}\nPrice for one: {Price}\nPrice for all: {Cost()}");
+    }
+}
